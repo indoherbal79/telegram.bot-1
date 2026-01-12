67f0c1f399f2ff27aa3d67f8a116b7ca5a591938
indoherbal79 <indoherbal79@gmail.com>
2026-01-12 22:59:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import requests
 import time
 
-TOKEN = 7432590124:AAE2WaUxe-xjkQ7a9Biwk1YWkGU_pFSCj24"
+TOKEN = "7432590124:AAE2WaUxe-xjkQ7a9Biwk1YWkGU_pFSCj24"
 CHAT_ID = "303839054"
 
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 def send(msg):
-    requests.post(URL, data={
-        "chat_id": CHAT_ID,
-        "text": msg
-    })
+    try:
+        requests.post(URL, data={"chat_id": CHAT_ID, "text": msg})
+    except Exception as e:
+        print("Gagal kirim pesan:", e)
 
 send("🤖 BOT ONLINE 24 JAM (RAILWAY)")
 
 while True:
+    send("🤖 BOT MASIH ONLINE")
     time.sleep(3600)
